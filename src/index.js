fcc9b83f13a2186886642659e1f2794c0e0c2754
horaciosystem <horacioalexandre.fernandes@gmail.com>
2016-02-29 21:50:50 -0300

    Implement video list and video list item

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyBnyfVihz2rttrJE2iuN-h_MtpH6_SntHA';
 
-const App = () => {
-  return (
-    <div>
-     <SearchBar />
-    </div>
-  );
-};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'oscar 2016'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+  render() {
+    return(
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
