0ce93519641f50bee1afe8d1f9c02673e0a82cc1
horaciosystem <horacioalexandre.fernandes@gmail.com>
2016-03-05 10:21:23 -0300

    Implement search bar action

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -12,8 +12,12 @@ export default class SearchBar extends Component {
       <div className="search-bar">
         <input
           value={this.state.term}
-          onChange={(event) => this.setState({ term: event.target.value })} />
+          onChange={(event) => this.onInputChange(event.target.value)} />
       </div>
     );
+  }
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChang(term);
   }
 };
